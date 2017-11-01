2e8b08525e569694f3d90b7e7e8e1ab561bce3fa
jiyi <jiyi_lari@yahoo.com>
2017-11-01 16:43:42 +0900

    put config back

---
+++ config.py
@@ -0,0 +1,16 @@
+PRICE_BUFFER_SIZE = 10
+
+WATCH_INTERVAL = 5 # second(s)
+THINK_INTERVAL = 5 # second(s)
+
+PREFILL_RETRY_INTERVAL = 500 # seconds
+
+DEBUG = True
+
+USERNAME = 'jiyi0327@gmail.com'
+PASSWORD = 'himitsu desu.'
+
+PHANTOM_BIN = './phantom/phantomjs'
+LEVEL_DB = './data/price-db'
+PRICE_FILE = './data/ETH_JPY.json'
+

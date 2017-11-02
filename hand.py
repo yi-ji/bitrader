11dbf2f0f3fa3b2c2b19ba14cb2fdb9b571e9385
jiyi <jiyi_lari@yahoo.com>
2017-11-02 18:29:36 +0900

    basic buy and sell tested

--- hand.py
+++ hand.py
@@ -1,10 +1,13 @@
 from utils import logger
-
+import time
+import config
+from selenium.common.exceptions import ElementNotVisibleException
 class Hand:
 	def __init__(self, _driver):
 		self.driver = _driver
 
 	def buy(self, price, eth=None, jpy=None):
+		logger.info('buying with price: '+str(price)+', amount '+str(eth)+' ether or '+str(jpy)+' JPY')
 		if eth is not None:
 			self.buy_by_eth(eth)
 		elif jpy is not None:
@@ -18,8 +21,21 @@ class Hand:
 		eth_input.send_keys(str(eth))
 		eth_buy = self.driver.find_element_by_id('buttonBUY')
 		eth_buy.click()
-		eth_buy_confirm = self.driver.find_element_by_id('doExecBuy')
-		eth_buy_confirm.click()
+		retry = 1
+		while True:
+			try:
+				time.sleep(1)
+				eth_buy_confirm = self.driver.find_element_by_id('doExecBuy')
+				eth_buy_confirm.click()
+				break
+			except ElementNotVisibleException:
+				if retry > config.TRADE_RETRY_MAX:
+					logger.warn('buying failed too many times, abort')
+					return
+				logger.debug('buying failed, retry')
+				retry += 1
+		time.sleep(5)
+		logger.info('bought '+str(eth)+' ether')
 
 	def sell(self, price, eth=None, jpy=None):
 		if eth is not None:
@@ -35,5 +51,18 @@ class Hand:
 		eth_input.send_keys(str(eth))
 		eth_sell = self.driver.find_element_by_id('buttonSELL')
 		eth_sell.click()
-		eth_sell_confirm = self.driver.find_element_by_id('doExecSell')
-		eth_sell_confirm.click()
+		retry = 1
+		while True:
+			try:
+				time.sleep(1)
+				eth_sell_confirm = self.driver.find_element_by_id('buttonSELL')
+				eth_sell_confirm.click()
+				break
+			except ElementNotVisibleException:
+				if retry > config.TRADE_RETRY_MAX:
+					logger.warn('selling failed too many times, abort')
+					return
+				logger.debug('selling failed, retry')
+				retry += 1
+		time.sleep(5)
+		logger.info('sold '+str(eth)+' ether')
--- me.py
+++ me.py
@@ -5,7 +5,10 @@ from selenium import webdriver
 
 class PriceBuffer:
 	def __init__(self):
-		self.buffer = collections.deque(config.PRICE_BUFFER_SIZE*['not ready'], config.PRICE_BUFFER_SIZE)
+		self.buffer = collections.deque([], config.PRICE_BUFFER_SIZE)
+		self.mid = None
+		self.ask = None
+		self.bid = None
 
 def signal_handler(signal, frame):
 	print('Terminating PhantomJS before exit')
@@ -15,7 +18,7 @@ def signal_handler(signal, frame):
 def init_web_driver(driver):
 	signal.signal(signal.SIGINT, signal_handler)
 	driver.set_window_size(1280, 1024)
-	logger.debug('opening login page')
+	logger.info('opening login page')
 	driver.get('https://bitflyer.jp/en-jp/ex/EthPrice')
 	#driver.save_screenshot('phantom/screen1.png')
 	email = driver.find_element_by_id('MainContent_email')
@@ -23,27 +26,29 @@ def init_web_driver(driver):
 	passwd = driver.find_element_by_id('MainContent_password')
 	passwd.send_keys(config.PASSWORD)
 	login = driver.find_element_by_id('MainContent_Button1')
-	logger.debug('ready for login')
+	logger.info('ready for login')
 	login.click()
 	#time.sleep(3)
 	#driver.save_screenshot('phantom/screen2.png')
 	driver.get('https://bitflyer.jp/en-jp/ex/EthPrice')
-	logger.debug('go to eth price page')
+	logger.info('go to eth price page')
 	time.sleep(3)
 	#driver.save_screenshot('phantom/screen3.png')
 
-
-price_buffer = PriceBuffer() # price data of latest period, multiple frequency and period length maybe
-
 driver = webdriver.PhantomJS(executable_path=config.PHANTOM_BIN)
-init_web_driver(driver)
 
-my_hand = hand.Hand(driver)
-my_eye = eye.Eye(price_buffer, driver)
-my_brain = brain.Brain(price_buffer, my_hand)
+def main():
+	price_buffer = PriceBuffer() # price data of latest period, multiple frequency and period length maybe
+	my_hand = hand.Hand(driver)
+	my_eye = eye.Eye(price_buffer, driver)
+	my_brain = brain.Brain(price_buffer, my_hand)
 
-my_eye.start_watching() # thread. collect data from website, put into (i) leveldb for future use; (ii) into buffer for brain to think about.
-my_brain.start_thinking() # read price data from buffer, send trading commands to hand.
+	my_eye.prefill()
 
+	init_web_driver(driver)
 
+	my_eye.start_watching() # thread. collect data from website, put into (i) leveldb for future use; (ii) into buffer for brain to think about.
+	time.sleep(3)
+	my_brain.start_thinking() # read price data from buffer, send trading commands to hand.
 
+main()

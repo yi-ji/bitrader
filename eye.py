143ebe36386276dc184d05fd4883fb651366afc8
jiyi <jiyi_lari@yahoo.com>
2017-11-30 11:21:39 +0900

    cookie refresh after getting 'CLOSED'

--- eye.py
+++ eye.py
@@ -1,13 +1,33 @@
 import config
 from utils import logger
-import time
-import threading
+import time, threading, sys, signal
 
 
 class Eye:
     def __init__(self, _memory, _driver):
         self.memory = _memory
         self.driver = _driver
+        self.closed_retry = 0
+
+    def init_driver(self):
+        def signal_handler(signal, frame):
+            logger.debug('Terminating PhantomJS before exit')
+            self.driver.quit()
+            sys.exit(0)
+        signal.signal(signal.SIGINT, signal_handler)
+        self.driver.set_window_size(1280, 1024)
+        logger.debug('opening login page')
+        self.driver.get('https://bitflyer.jp/login')
+        email = self.driver.find_element_by_id('MainContent_email')
+        email.send_keys(config.USERNAME)
+        passwd = self.driver.find_element_by_id('MainContent_password')
+        passwd.send_keys(config.PASSWORD)
+        login = self.driver.find_element_by_id('MainContent_Button1')
+        logger.debug('ready for login')
+        login.click()
+        self.driver.get('https://bitflyer.jp/en-jp/ex/EthPrice')
+        logger.debug('go to eth price page')
+        time.sleep(5)
 
     def get_eth_price(self, Ask_or_Bid):
         return self.driver.find_element_by_css_selector('strong.bfPrice' + Ask_or_Bid).text.replace(',', '')
@@ -19,6 +39,7 @@ class Eye:
         return self.driver.find_element_by_id('ETHAmount_pw').text
 
     def start_watching(self):
+        self.init_driver()
         time.sleep(5)
         threading.Thread(target=self.watch).start()
 
@@ -27,6 +48,11 @@ class Eye:
             ask_price = self.get_eth_price('Ask')
             bid_price = self.get_eth_price('Bid')
             if ask_price == 'CLOSED' or bid_price == 'CLOSED':
+                logger.debug('showing CLOSED')
+                self.closed_retry += 1
+                if self.closed_retry > 10:
+                    self.init_driver()
+                    self.closed_retry = 0
                 time.sleep(1)
                 continue
             balance_eth = self.get_balance_eth()
